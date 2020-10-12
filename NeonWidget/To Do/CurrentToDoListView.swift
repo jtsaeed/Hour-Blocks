adad26ad99139a1fb6d704b22a5ad25fdd5a72d1
James Litchfield <james.litchfield99@gmail.com>
2020-10-12 10:56:25 +0100

    create basic layout

--- NeonWidget/To Do/CurrentToDoListView.swift
+++ NeonWidget/To Do/CurrentToDoListView.swift
@@ -9,21 +9,62 @@
 import SwiftUI
 
 struct CurrentToDoListView: View {
+    let todos: [ToDoItem]?
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .bottomLeading) {
+            LinearGradient(gradient: Gradient(colors: [Color("GradientEnd"), Color("UrgentColor")]),
+                           startPoint: .top,
+                           endPoint: .bottom)
+            
+            
+            if let todos = todos {
+                VStack(alignment: .leading, spacing: 4) {
+                    ForEach(todos, id: \.self.id) { todo in
+                        Text(todo.title)
+                            .font(.system(size: 17, weight: .semibold, design: .default))
+                    }
+                }
+                .padding(16)
+                .foregroundColor(.white)
+                
+            }else {
+                Text("No todo items today")
+                    .font(.system(size: 24, weight: .semibold, design: .default))
+            }
+            
+            
+            
+        }
     }
 }
 
 struct PlaceholderCurrentToDoListView: View {
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .bottomLeading) {
+            LinearGradient(gradient: Gradient(colors: [Color("GradientEnd"), Color("UrgentColor")]),
+                           startPoint: .top,
+                           endPoint: .bottom)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                
+                Text("Clear out fridge.")
+                    .font(.system(size: 17, weight: .semibold, design: .default))
+                    .redacted(reason: .placeholder)
+                
+                Text("Tidy room.")
+                    .font(.system(size: 17, weight: .semibold, design: .default))
+                    .redacted(reason: .placeholder)
+                
+            }.padding(16)
+            .foregroundColor(.white)
+        }
     }
 }
 
 struct CurrentToDoListView_Previews: PreviewProvider {
     static var previews: some View {
-        CurrentToDoListView()
+        CurrentToDoListView(todos: [])
     }
 }
